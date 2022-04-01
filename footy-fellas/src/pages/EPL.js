58b467ebe52bac4374c091fc19c55847787f71d3
Max W <mwinter@wesleyan.edu>
2022-03-31 21:00:34 -0500

    starting to grab EPL standings from API

---
+++ footy-fellas/src/pages/EPL.js
@@ -0,0 +1,15 @@
+import React from "react";
+import standingList from "../components/EPLStandings";
+
+
+const EPL = () => {
+
+
+    return (
+        <section>
+            
+        </section>
+    );
+};
+
+export default EPL;
