d79d634890db0af0a7b1e78d38d5702470dd2773
Max W <mwinter@wesleyan.edu>
2022-04-03 19:18:54 -0500

    minor change in server

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../footy-fellas/build/index.html"));
+    res.sendFile(path.join(__dirname, "../footy-fellas/public/index.html"));
 });
 
 db.on("error", () => {
