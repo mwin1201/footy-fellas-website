e1524cb69ca8a39aa07e83dac525aa6f77b4ec76
Max W <mwinter@wesleyan.edu>
2022-04-22 20:51:41 -0500

    started signup page

---
+++ footy-fellas/src/pages/Signup.js
@@ -0,0 +1,51 @@
+import React, { useState } from react;
+import { useMutation } from "@apollo/client";
+import { validateEmail } from "../utils/helpers";
+
+
+const Signup = () => {
+
+    const [formState, setFormState] = useState(
+        {
+            email: "",
+            password: ""
+        }
+    );
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleUserSignup = async (event) => {
+        event.preventDefault();
+        
+        const email = document.getElementById("email").value;
+        const isValid = validateEmail(email);
+        
+    };
+
+    return (
+        <section>
+            <form onSubmit={handleUserSignup}>
+                <div>
+                    <h1>Join the Fellas!</h1>
+
+                    <label htmlFor="email">Email:</label>
+                    <input id="email" name="email" type="email" onBlur={handleChange}></input>
+
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" name="password" type="password" onBlur={handleChange}></input>
+                </div>
+
+                <button>Submit</button>
+            </form>
+        </section>
+    );
+};
+
+export default Signup;
--- footy-fellas/src/utils/helpers.js
+++ footy-fellas/src/utils/helpers.js
@@ -9,3 +9,8 @@ const nameCheck = (team) => {
 };
 
 export default nameCheck;
+
+export function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
